bfeae107bcf4edf237dd616f48a3b02c9e972545
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-06-20 12:17:18 +0300

    changes

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -23,7 +23,7 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hoodapp.urls')),
-    path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
+    # path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('logout/', views.LogoutView.as_view(), {'next_page': '/'}),
